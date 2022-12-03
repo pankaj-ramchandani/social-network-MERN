c219b3f72ef3250f2eb43c63ba964cb4b13ecee7
“Pankaj” <ramchandanipankaj787@gmail.com>
2022-12-03 08:23:07 +0530

    git init

---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    min: 2,
+    max: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 50,
+    unique: true,
+  },
+});
